93334180f9c72bc870b8e592b50adc36980baffb
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-04-30 23:06:31 +0530

    new feature:save working

--- src/components/ScholarshipCard.jsx
+++ src/components/ScholarshipCard.jsx
@@ -1,35 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function ScholarshipCard(prop) {
-    // console.log(prop);
-    const [bookMarkList,setBookMarkList]=useState([])
-    return (
-        <div>
-            <Link to={`/scholarshiplist/${prop.items.name}`}>
-                <div className='border w-auto flex'>
-                    
-                    <div id={prop.items.id}>
-                        <p>country name:{prop.countryName.country}</p>
-                        <p>university name:{prop.universityName.name}</p>
-                        <p className='font-bold'>scholarshipName: {prop.items.name}</p>
-                        <p>DegreeLevel: {prop.items.degreeLevel}</p>
-                        <p>amount: {prop.items.amount}</p>
-                        <p>deadline:{prop.items.deadline}</p>
-                    </div>
-                    <div className=''>
-                        {/* <span className="material-symbols-outlined "
-                        onClick={addToBookMark}>
-                            bookmark
-                        </span> */}
-
-                    </div>
-
-                </div>
-            </Link>
+  // console.log(prop);
+ 
+  return (
+    <div>
+      <Link to={`/scholarshiplist/${prop.scholarshipName}`}>
+        <div className="border w-auto flex ">
+          <div>
+            <p>country name:{prop.countryName}</p>
+            <p>university name:{prop.universityName}</p>
+            <p className="font-bold">scholarshipName: {prop.scholarshipName}</p>
+            <p>DegreeLevel: {prop.degreeLevel}</p>
+            <p>amount: {prop.amount}</p>
+            <p>deadline:{prop.deadline}</p>
+          </div>
         </div>
-    )
+      </Link>
+      <div className="">
+        <span
+          className="material-symbols-outlined "
+          onClick={() => prop.onClick(prop.id)}
+        >
+          bookmark
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default ScholarshipCard
+export default ScholarshipCard;
--- src/pages/ScholarShipList.jsx
+++ src/pages/ScholarShipList.jsx
@@ -1,84 +1,99 @@
-import React, { useEffect } from 'react'
-import scholarshipsData from '../data'
-import ScholarshipCard from '../components/ScholarshipCard';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import scholarshipsData from "../data";
+import ScholarshipCard from "../components/ScholarshipCard";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function ScholarShipList() {
-  const [dataInitial,setDataInitial]=useState([])
-  const [isFiltered,setIsFiltered]=useState(false);
-  const [filteredData,setFilteredData]=useState([])
+  const [dataInitial, setDataInitial] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [filteredData, setFilteredData] = useState([]);
+  const [bookMarkList, setBookMarkList] = useState([]);
 
 
-    // const data = 
+  useEffect(() => {
+    setDataInitial(scholarshipsData);
+  }, []);
 
-
-
-  useEffect(()=>{
-    setDataInitial(scholarshipsData)
-  },[])
- 
   // console.log(scholarshipsData);
+  function toggleBookMark(id){
+    console.log(id)
+  }
 
-  function submitData(e){
+  function submitData(e) {
     e.preventDefault();
     setIsFiltered(true);
 
-    const formData=new FormData(e.currentTarget)
+    const formData = new FormData(e.currentTarget);
 
-    const country=formData.get('country');
-    const universityName=formData.get('universityName');
-    const type=formData.get('type');
-    console.log(country,universityName,type)
-    console.log(Object.fromEntries(formData))
-     const countrySelect=scholarshipsData.filter((item)=>(
-      item.country===country
-     ))
-     console.log(countrySelect[0])
-     console.log(countrySelect[0].country)
+    const country = formData.get("country");
+    const universityName = formData.get("universityName");
+    const type = formData.get("type");
+    console.log(country, universityName, type);
+    console.log(Object.fromEntries(formData));
+    const countrySelect = scholarshipsData.filter(
+      (item) => item.country === country
+    );
+    console.log(countrySelect[0]);
+    console.log(countrySelect[0].country);
 
-     const universitySelect=countrySelect[0].universities.filter((item)=>item.name===universityName)
-     console.log(universitySelect)
+    const universitySelect = countrySelect[0].universities.filter(
+      (item) => item.name === universityName
+    );
+    console.log(universitySelect[0].name);
 
-     const scholarshipType=universitySelect[0].scholarships.filter((item)=>item.degreeLevel===type)
-     console.log(scholarshipType);
+    const scholarshipType = universitySelect[0].scholarships.filter(
+      (item) => item.degreeLevel === type
+    );
+    console.log(scholarshipType);
 
-     const fullScholarshipData = scholarshipType.map((sch) => ({
+    const fullScholarshipData = scholarshipType.map((sch) => ({
       items: sch,
-      universityName: universitySelect,
-      countryName: countrySelect
+      universityName: universitySelect[0].name,
+      countryName: countrySelect[0].country,
     }));
-  
+    console.log(fullScholarshipData);
+
     setFilteredData(fullScholarshipData);
-    setIsFiltered(true);
-    
+
+    console.log(filteredData);
   }
   // console.log(filteredData);
   console.log(dataInitial);
 
-
   return (
-    <div>     
+    <div>
       <form action="" onSubmit={submitData}>
-      <select name="country" id="">
+        <select name="country" id="">
           <option value="Singapore">Singapore</option>
           <option value="Canada">Canada</option>
           <option value="United States">United States</option>
           <option value="Australia">Australia</option>
         </select>
-        <select name="universityName" id="" >
-          <option value="National University of Singapore (NUS)">National University of Singapore</option>
-          <option value="Nanyang Technological University (NTU)">Nanyang Technological University</option>
+        <select name="universityName" id="">
+          <option value="National University of Singapore (NUS)">
+            National University of Singapore
+          </option>
+          <option value="Nanyang Technological University (NTU)">
+            Nanyang Technological University
+          </option>
           <option value="University of Toronto">University of Toronto</option>
-          <option value="University of British Columbia (UBC)">University of British Columbia</option>
-          <option value="Technical University of Munich (TUM)">Technical University of Munich</option>
+          <option value="University of British Columbia (UBC)">
+            University of British Columbia
+          </option>
+          <option value="Technical University of Munich (TUM)">
+            Technical University of Munich
+          </option>
           <option value="RWTH Aachen University">RWTH Aachen University</option>
           <option value="Harvard University">Harvard University</option>
           <option value="Stanford University">Stanford University</option>
-          <option value="University of Melbourne">University of Melbourne</option>
+          <option value="University of Melbourne">
+            University of Melbourne
+          </option>
           <option value="University of Sydney">University of Sydney</option>
         </select>
-        <select name="type" id="" >
+        <select name="type" id="">
           <option value="Master's">Master's</option>
           <option value="PhD">PhD</option>
           <option value="Master's/PhD">Master's/PhD</option>
@@ -86,34 +101,50 @@ function ScholarShipList() {
           <option value="">All Graduate Programs</option>
         </select>
         <button>Submit</button>
-
       </form>
-      <button onClick={()=>setIsFiltered(false)}>clear</button>
+      <button onClick={() => setIsFiltered(false)}>clear</button>
 
-      <div className='grid grid-cols-5 gap-5 '>
-        {isFiltered ? 
-
-         filteredData.map((data,index) =>
-         <ScholarShipList 
-           items={data.items} countryName={data.country} universityName={data.universitySelect} id={items}/>
-         ) :
-         dataInitial.map((countryName) =>
-          countryName.universities.map((universityName) => (
-            universityName.scholarships.map((items) => (
-              // console.log(items)
-              
-               <ScholarshipCard universityName={universityName} countryName={countryName} items={items} id={items}/>
-              
+      <div className="grid grid-cols-5 gap-5 ">
+        {isFiltered
+          ? filteredData.map((data, index) => (
+              // console.log(data)
+              <ScholarshipCard
+                key={index}
+                id={data.items.id}
+                //  items={data.items}
+                countryName={data.countryName}
+                universityName={data.universityName}
+                amount={data.items.amount}
+                deadline={data.items.deadline}
+                scholarshipName={data.items.name}
+                degreeLevel={data.items.degreeLevel}
+                onClick={toggleBookMark}
+              />
             ))
-          ))
-        )
-         
-        }
+          : dataInitial.map((countryName) =>
+            // console.log(countryName.country)
+              countryName.universities.map((universityName) =>
+                // console.log(universityName)
+                universityName.scholarships.map((items, index) => (
+                  // console.log(items)
+
+                  <ScholarshipCard
+                    key={index}
+                    id={items.id}
+                    universityName={universityName.name}
+                    countryName={countryName.country}
+                    amount={items.amount}
+                    deadline={items.deadline}
+                    scholarshipName={items.name}
+                    degreeLevel={items.degreeLevel}
+                    onClick={toggleBookMark}
+                  />
+                ))
+              )
+            )}
       </div>
     </div>
-
-  )
+  );
 }
 
-export default ScholarShipList
-
+export default ScholarShipList;
