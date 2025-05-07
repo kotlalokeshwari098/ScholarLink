2451df51985570bcf0185b15db39a7123c243e7f
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-05-07 13:47:36 +0530

    auth setting up +cors

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function AboutUs() {
+    const [data,setData]=useState()
+  axios.get('http://localhost:5000/')
+  .then(res=>console.log(res.data))
+  .catch(err => console.error('Error:', err))
+
+
+
   return (
     <div>
-      about us page
+      
     </div>
   )
 }
--- client/src/pages/BookMark.jsx
+++ client/src/pages/BookMark.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import {Stack,IconButton,Typography,Box} from "@mui/material"
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { myBookmark } from './ScholarShipList';
 import { useContext } from 'react';
 
@@ -10,14 +8,7 @@ function BookMark() {
   console.log(data);
   return (
     <>
-       <Stack direction='row' alignItems="center">
-          <IconButton sx={{color:'black' , mr:1}}>
-                   <ArrowBackIcon />
-          </IconButton>
-          <Typography variant='h6'>
-            Bookmarks
-          </Typography>
-       </Stack>
+       
        <div>
         
        </div>
